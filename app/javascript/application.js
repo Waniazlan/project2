ea6617889622c97bdd4804f3a2746af9d52d22f1
Waniazlan <waniazlan27@gmail.com>
2024-09-06 17:51:00 +0800

    new commit

---
+++ app/javascript/application.js
@@ -0,0 +1,14 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "popper"
+import "bootstrap"
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
